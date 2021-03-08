8cf4acbe9bdf5497d4cb8cdbd46e754ddbfae8fc
techtheriac <mavyjayann@gmail.com>
2021-03-08 14:32:24 +0100

    Integrate with Strapi

---
+++ pages/musings/[slug].js
@@ -0,0 +1,30 @@
+import * as S from "sanctuary";
+
+export default function Post({ post }) {
+  return <p>{post.content}</p>;
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${process.env.URL}/posts`);
+  const posts = await res.json();
+
+  const paths = S.map((post) => ({ params: { slug: post.slug } }))(posts);
+
+  return {
+    paths,
+    // for incremental static generation, change to true
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const res = await fetch(`${process.env.URL}/posts?slug=${slug}`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post: post[0],
+    },
+  };
+}
--- pages/musings/index.js
+++ pages/musings/index.js
@@ -1,12 +1,32 @@
-import styles from "../styles/Musings.module.scss";
+import styles from "../../styles/Musings.module.scss";
 import Head from "next/head";
-
+import Link from "next/link";
 import AltNavigation from "../../components/navigation/AltNavigation";
+import * as S from "sanctuary";
 
-export default function Musings() {
+export default function Musings({ posts }) {
+  const BlogListing = (x) => (
+    <div key={x.id}>
+      <Link href={`/musings/${x.slug}`}>
+        <a>
+          <h1>{x.title}</h1>
+        </a>
+      </Link>
+    </div>
+  );
+
   return (
     <div className={styles.containerMusings}>
       <AltNavigation />
+      {S.map(BlogListing)(posts)}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.URL}/posts`);
+  const posts = await res.json();
+  return {
+    props: { posts },
+  };
 }
