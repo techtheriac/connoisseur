aca9bf23d918fc84f5a5d3e0c61e73a9bf190f25
techtheriac <mavyjayann@gmail.com>
2021-03-18 17:40:33 +0100

    comment out musings/slug

--- pages/musings/[slug].js
+++ pages/musings/[slug].js
@@ -1,30 +1,30 @@
-import * as S from "sanctuary";
+// import * as S from "sanctuary";
 
-export default function Post({ post }) {
-  return <p>{post.content}</p>;
-}
+// export default function Post({ post }) {
+//   return <p>{post.content}</p>;
+// }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${process.env.URL}/posts`);
-  const posts = await res.json();
+// export async function getStaticPaths() {
+//   const res = await fetch(`${process.env.URL}/posts`);
+//   const posts = await res.json();
 
-  const paths = S.map((post) => ({ params: { slug: post.slug } }))(posts);
+//   const paths = S.map((post) => ({ params: { slug: post.slug } }))(posts);
 
-  return {
-    paths,
-    // for incremental static generation, change to true
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     // for incremental static generation, change to true
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const { slug } = params;
-  const res = await fetch(`${process.env.URL}/posts?slug=${slug}`);
-  const post = await res.json();
+// export async function getStaticProps({ params }) {
+//   const { slug } = params;
+//   const res = await fetch(`${process.env.URL}/posts?slug=${slug}`);
+//   const post = await res.json();
 
-  return {
-    props: {
-      post: post[0],
-    },
-  };
-}
+//   return {
+//     props: {
+//       post: post[0],
+//     },
+//   };
+// }
