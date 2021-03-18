5500d1ea45827df205bc8e2f0a51aea4b632e1e3
techtheriac <mavyjayann@gmail.com>
2021-03-18 17:30:29 +0100

    Add Under construction text

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className={styles}>
-      <Header />
+    <div className={styles.underConstruction}>
+      <h2>Chill out!! Under construction</h2>
     </div>
   );
 }
