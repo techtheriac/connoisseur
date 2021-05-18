3ef66023be13a2927f2d59cf3ad6ccbed335d180
techtheriac <mavyjayann@gmail.com>
2021-05-18 16:27:58 +0100

    show image on mobile

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
 const path = require("path");
-// const withMDX = require("@next/mdx")({
-//   extension: /\.mdx$/,
-// });
-
-// module.exports = withMDX({
-//   pageExtensions: ["js", "mdx"],
-//   sassOptions: {
-//     includePaths: [path.join(__dirname, "styles")],
-//   },
-//   webpack: function (config) {
-//     config.module.rules.push({ test: /\.md$/, use: "raw-loader" });
-//     config.module.rules.push({ test: /\.yml$/, use: "raw-loader" });
-//     config.module.rules.push({
-//       test: /\.(glsl|frag|vert)$/,
-//       use: ["raw-loader", "glslify-loader"],
-//       exclude: /node_modules/,
-//     });
-//     return config;
-//   },
-// });
-
 module.exports = {
-  //pageExtensions: ["js", "mdx"],
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
