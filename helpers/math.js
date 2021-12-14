896a091a8968303e80d70d12cf37cf250a0e67d1
techtheriac <mavyjayann@gmail.com>
2021-12-14 12:42:00 +0100

    Purge unused js

--- helpers/math.js
+++ helpers/math.js
@@ -6,3 +6,7 @@ export function random(min, max) {
 export function map(n, start1, end1, start2, end2) {
   return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
 }
+
+export function lerp(start, end, t) {
+  return start * (1 - t) + end * t;
+}
