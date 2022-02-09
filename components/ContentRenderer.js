72992ad8072ff71a31e43844075a4f02956ca82b
Franklin Jezreel <mavyjayann@gmail.com>
2022-02-09 13:46:37 +0100

    Generalize rendering functions

--- components/ContentRenderer.js
+++ components/ContentRenderer.js
@@ -1,28 +1,68 @@
+// TO-DO - Get a sorted sematic structure of blog post content before rendering 
 import {styled} from "@stitches/react";
 import * as R from 'ramda';
 
+const Map = R.addIndex(R.map);
+
 const Paragraph = styled("p", {
-  fontSize: "1.3rem",
+  fontSize: "1.2rem",
 });
 
 const HeadingOne = styled("h1");
+const HeadingTwo = styled("h2");
+const HeadingThree = styled("h3");
+const HeadingFour = styled("h4");
+const HeadingFive = styled("h5");
+const HeadingSix = styled("h6");
 
-const CONTENT_TYPE_PATH = R.lensPath(["type"])
+const ELEMENT_TYPE_PATH = R.lensPath(["type"])
 const PARAGRAPH_CONTENT_PATH = R.lensPath(["paragraph", "text", 0, "plain_text"])
+const PARAGRAPH_PATH = R.lensPath(["paragraph"]);
 const HEADING_CONTENT_PATH = (level) => R.lensPath([`heading_${level}`, "text", 0, "plain_text"])
 
-const getElementType = (path, elementObject) => R.view(path, elementObject);
-const getParagraphContent = (path, elementObject) => R.view(path, elementObject);
-const getHeadingContent = (path, elementObject) => R.view(path, elementObject);
+const getContent = (path) => (elementObject) => R.view(path, elementObject);
+const elementHasChildren = (path) => (elementObject) => R.length(R.view(path, elementObject)) > 1 
 
-const renderProcedure = (elementObject) => {
-  if(getElementType(CONTENT_TYPE_PATH, elementObject) == "paragraph") {
-    return <Paragraph key={elementObject.id}>{getParagraphContent(PARAGRAPH_CONTENT_PATH, elementObject)}</Paragraph>
-  }
+const getParagraphContent = getContent(PARAGRAPH_CONTENT_PATH);
+const getParagraphArray = getContent(PARAGRAPH_PATH);
+const getElementType = getContent(ELEMENT_TYPE_PATH);
+const paragraphHasChildren = elementHasChildren(PARAGRAPH_PATH);
 
-  if(getElementType(CONTENT_TYPE_PATH, elementObject) == "heading_1") {
-    return <HeadingOne key={elementObject.id}>{getHeadingContent(HEADING_CONTENT_PATH(1), elementObject)}</HeadingOne>
+const parseParagraph = (elementObject, index) => {
+  // Use switch statment to capture every possible condition declaratively
+  if(!paragraphHasChildren(elementObject)) {
+    return <Paragraph key={index}>{getParagraphContent(elementObject)}</Paragraph>
   }
 }
 
-export const ContentRenderer = ({postContent}) => <div> { R.map(renderProcedure, postContent) }</div>;
+const renderProcedure = (elementObject, index) => {
+  if(getElementType(elementObject) == "paragraph") {
+    return parseParagraph(elementObject, index)
+  }
+
+  if(getElementType(elementObject) == "heading_1") {
+    return (
+        <HeadingOne key={index}>
+          {getContent(HEADING_CONTENT_PATH(1))(elementObject)}
+        </HeadingOne>
+    );
+  }
+
+  if(getElementType(elementObject) == "heading_2") {
+    return (
+      <HeadingTwo key={index}>
+        {getContent(HEADING_CONTENT_PATH(2))(elementObject)}
+      </HeadingTwo>
+    );
+  }
+
+  if(getElementType(elementObject) == "heading_3") {
+    return (
+      <HeadingTwo key={index}>
+        {getContent(HEADING_CONTENT_PATH(3))(elementObject)}
+      </HeadingTwo>
+    );
+  }
+}
+
+export const ContentRenderer = ({postContent}) => <div> { Map(renderProcedure, postContent) }</div>;
