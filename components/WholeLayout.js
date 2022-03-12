7aa1317323e78d74a2bb2e923401fd44871c84ee
Franklin Jezreel <mavyjayann@gmail.com>
2022-03-12 17:56:18 +0100

    Update scroll api

--- components/WholeLayout.js
+++ components/WholeLayout.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import ThreeWrapper from "@/components/ThreeWrapper";
 import Navigation from "./Navigation";
 import styles from "@/styles/Home.module.scss";
-import ASScroll from "@ashthornton/asscroll";
+//import ASScroll from "@ashthornton/asscroll";
+import { SmoothScroll } from "@emotionagency/smoothscroll";
 
 const WholeLayout = ({ children }) => {
   const candyConfig = {
@@ -11,16 +12,20 @@ const WholeLayout = ({ children }) => {
   };
 
   useEffect(() => {
-    const asscroll = new ASScroll({ ease: 0.075, disableRaf: false });
-    window.addEventListener("load", () => {
-      asscroll.enable({ reset: 0 });
-    });
+    // const asscroll = new ASScroll({ ease: 0.075, disableRaf: false });
+    // window.addEventListener("load", () => {
+    //   asscroll.enable({ reset: 0 });
+    // });
+    // asscroll.on("scroll", (scrollPos) => console.log(scrollPos));
 
-    asscroll.on("scroll", (scrollPos) => console.log(scrollPos));
+    const scroll = new SmoothScroll({ scrollbar: false });
+    scroll.reset();
+
+    window.addEventListener("resize", () => scroll.reset());
   });
 
   return (
-    <div asscroll-container="true">
+    <div id="scroll-container">
       <ThreeWrapper candyConfig={candyConfig}>
         <main className={styles.containerMain}>
           {/* <Navigation /> */}
