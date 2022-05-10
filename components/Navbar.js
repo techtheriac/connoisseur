905f032d201aadf43d4c5d0a083bd8bceaab0339
Franklin Jezreel <mavyjayann@gmail.com>
2022-05-10 19:38:31 +0100

    Implement css transition

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,6 @@ export default function Navbar() {
       }}
     >
       <Navigation />
-      {/* <Filter /> */}
     </_Navbar>
   );
 }
--- components/TitlesHome.js
+++ components/TitlesHome.js
@@ -2,13 +2,10 @@ import { styled, css } from "stitches.config";
 import TitleHomeItem from "./TitleHomeItem";
 import React from "react";
 import { useTagsContext } from "./TagsProvider";
+
 const parseUrl = (tags, slug) => {
   return tags.includes("poetry") ? `/poetry/${slug}` : `/musings/${slug}`;
 };
-
-// Hold an array that filters out certain tags onclick
-// If the array contains tags that matches the tags of a cetain `TitleHomeItem`
-// Let the item be visible
 
 const handleVisibilityToggle = (itemTags, selectedTag) => {
   return itemTags.includes(selectedTag) ? "active" : "inactive";
